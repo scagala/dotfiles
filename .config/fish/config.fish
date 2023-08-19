fe8edef5ae2b9d377f3483cc9b9d15b117c7a2af
Simon Cagala <simon.cagala@gmail.com>
2023-08-19 22:15:00 +0200

    added fish config

---
+++ .config/fish/config.fish
@@ -0,0 +1,25 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
+set -g fish_greeting
+
+#####################################
+#               ALIASES             #
+#####################################
+
+# SYS
+alias ls="exa --oneline --icons --header"
+alias la="exa --long --all --icons --no-user --git --octal-permissions --no-permissions --header"
+alias ltr="exa --tree --icons"
+alias ltd="exa --tree --only-dirs --icons"
+# GIT
+alias gs="git status"
+alias ga="git add"
+alias gc="git commit"
+alias gp="git push"
+alias gpp="git pull"
+# OPTIMUS
+alias gpu="optimus-manager --print-mode"
+alias amd="optimus-manager --switch integrated"
+alias nvidia="optimus-manager --switch nvidia"

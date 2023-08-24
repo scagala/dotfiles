3495f76875a36680ce27f853ce99fb3317a3ea60
Simon Cagala <simon.cagala@gmail.com>
2023-08-24 19:57:40 +0200

    Moved qtile colorscheme to separate file

---
+++ .config/qtile/custom/colorscheme.py
@@ -0,0 +1,25 @@
+colors = {
+    # darkest to lightest
+    "crust": "#121926",
+    "mantle": "#1e2030",
+    "base": "#24273a",
+    "surface" : "#363a4f",
+    "overlay" : "#6e738d",
+    "subtext" : "#a5adcb",
+    "text" : "#cad3f5",
+    # colors
+    "lavender" : "#b7bdf8",
+    "blue" : "#8aadf4",
+    "sapphire" : "#7dc4e4",
+    "sky" : "#91d7e3",
+    "teal" : "#8bd5ca",
+    "green" : "#a6da95",
+    "yellow" : "#eed49f",
+    "peach" : "#ef9f76",
+    "maroon" : "#ee99a0",
+    "red" : "#ed8796",
+    "mauve" : "#c6a0f6",
+    "pink" : "#f5bde6",
+    "flamingo" : "#f0c6c6",
+    "rosewater" : "#f4dbd6"
+}

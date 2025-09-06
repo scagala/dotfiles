d951114b174ff5baf1bf05be71ed9aeb6ed83594
Simon Cagala <simon.cagala@protonmail.com>
2025-09-06 22:20:49 +0200

    add ohmyposh config

--- fish/config.fish
+++ fish/config.fish
@@ -4,4 +4,4 @@ fish_ssh_agent
 # starship init fish | source
 zoxide init fish | source
 # oh-my-posh init fish --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/refs/heads/main/themes/amro.omp.json' | source
-oh-my-posh init fish --config 'https://github.com/JanDeDobbeleer/oh-my-posh/blob/main/themes/emodipt-extend.omp.json' | source
+oh-my-posh init fish --config $HOME/.config/oh-my-posh.omp.json | source

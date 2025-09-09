265fb98c2b56be75abf312fe84fa6d708f9da44e
Simon Cagala <simon.cagala@protonmail.com>
2025-09-09 20:53:28 +0200

    mass dump

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -6,4 +6,4 @@ fish_ssh_agent
 # starship init fish | source
 zoxide init fish | source
 # oh-my-posh init fish --config 'https://raw.githubusercontent.com/JanDeDobbeleer/oh-my-posh/refs/heads/main/themes/amro.omp.json' | source
-oh-my-posh init fish --config $HOME/.config/oh-my-posh.omp.json | source
+oh-my-posh init fish --config $HOME/.config/oh-my-posh/config.omp.json | source
